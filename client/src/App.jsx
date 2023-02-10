8581f9c72c80ff9f12a967969dddf6fa11563a5f
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-10 21:01:42 +0545

    routes set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import ProfilePage from "./scenes/profilePage";
+import LoginPage from "./scenes/loginPage";
+
 function App() {
-  return <div className="app">adsgads</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
