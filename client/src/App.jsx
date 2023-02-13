af85a608aa15be6eb532be2d457bb7a7a25c02a0
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-13 12:18:44 +0545

    login and register page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode); // get mode from redux store
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); // useMemo to prevent unnecessary re-renders
+  const isAuth = Boolean(useSelector((state) => state.token)); // get user from redux store
 
   return (
     <div className="app">
@@ -20,8 +21,14 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
