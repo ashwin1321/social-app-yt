16a092f62386817f55397e1f5f0f8ad1583c3eef
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-13 09:29:54 +0545

    navbar partially done

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  // const fullName = `${user.FirstName} ${user.lastName}`;
+  // const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
     <div>
