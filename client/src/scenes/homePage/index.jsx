cc4492d34453c1d934d6dc44e109aa1e3b7df2ff
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-14 13:42:16 +0545

    user profile widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,11 +1,33 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import UserWidget from "../widgets/userWidget";
 import Navbar from "../navbar";
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(Min-width: 1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "30%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        ></Box>
+
+        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+      </Box>
     </Box>
   );
 };
