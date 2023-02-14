33db3590a90acaf3d3e0f665ce3a18c85fb7bc2b
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-14 21:17:37 +0545

    posts

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import UserWidget from "../widgets/userWidget";
 import Navbar from "../navbar";
 import MyPostWidget from "../widgets/MyPostWidget.jsx";
+import PostsWidget from "../widgets/PostsWidget.jsx";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(Min-width: 1000px)");
@@ -18,16 +19,23 @@ const HomePage = () => {
         gap="0.5rem"
         justifyContent="space-between"
       >
-        <Box flexBasis={isNonMobileScreens ? "30%" : undefined}>
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
           <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
-
         <Box
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
-        ></Box>
-        <MyPostWidget picturePath={picturePath} />
-        {isNonMobileScreens && <Box flexBasis="30%"></Box>}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            {/* <AdvertWidget /> */}
+            <Box m="2rem 0" />
+            {/* <FriendListWidget userId={_id} /> */}
+          </Box>
+        )}
       </Box>
     </Box>
   );
